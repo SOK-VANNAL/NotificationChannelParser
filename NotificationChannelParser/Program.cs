b07a6b30dea159a52f929372960f7e488dde9f48
Vannal SOK <vannal.sok@chipmong.com>
2024-06-13 13:41:02 +0700

    First Commit

---
+++ NotificationChannelParser/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using System.Text.RegularExpressions;
+
+while (true)
+{
+    Console.Write("Input: ");
+    var title = Console.ReadLine();
+    var validChannel = new List<string> { "BE", "FE", "QA", "Urgent" };
+    var tags = Regex.Matches(title, @"\[([^]]+)\]").Select(x => x.Groups[1].Value).ToList();
+    var channels = new List<string?>();
+    foreach (var tag in tags)
+    {
+        if (validChannel.Contains(tag))
+        {
+            channels.Add(tag);
+        }
+    }
+    Console.WriteLine($"Output: Receive channels: {string.Join(", ", channels)}");
+    Console.WriteLine("");
+}
